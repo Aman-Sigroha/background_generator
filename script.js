b34efc70efdb90b5b425811d8124a5ef4900c574
Aman-Sigroha <amansigroha4@gmail.com>
2024-01-19 19:15:27 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,45 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomColor = document.querySelector(".randomColor");
+
+const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+
+function getCharacter(index) {
+	return hexCharacters[index]
+}
+
+function generateNewColor() {
+	let hexColorRep = "#"
+
+	for (let index = 0; index < 6; index++){
+		const randomPosition = Math.floor ( Math.random() * hexCharacters.length ) 
+    	hexColorRep += getCharacter( randomPosition )
+	}
+	
+	return hexColorRep
+}
+
+function setRandomGradient(){
+	color1.value = generateNewColor();
+	color2.value = generateNewColor();
+	setGradient();
+}
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+randomColor.addEventListener("click", setRandomGradient);
